cc6c17022e2605ef0b1b90a289315e5527a2722f
jaotarzan <joaovssouza59@gmail.com>
2025-04-22 16:51:15 -0300

    FEAT: Create Global Button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
+<script setup>
+import { GlobalButton } from '@/components'
+</script>
 
 <template>
   <main>
     <h1 class=" text-6xl">HELLO GUYS, FIRST WE NEED TO DOWNLOAD DJANGO</h1>
+    <GlobalButton />
 </main>
 </template>
