7260dbd76302da606991bf92e866c5f33cb16f0e
Gabz047 <gabe1612mail@gmail.com>
2025-04-23 13:20:13 -0300

    FEAT: Created product card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
+<script setup>
+import { ProductCard } from '@/components';
+</script>
 
 <template>
-  <main>
-    <h1 class=" text-6xl">HELLO GUYS, FIRST WE NEED TO DOWNLOAD DJANGO</h1>
-</main>
+  <ProductCard />
 </template>
