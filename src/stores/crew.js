2fa22b87215a438e93657da2c08faeac82b009b1
jaotarzan <joaovssouza59@gmail.com>
2025-04-26 17:53:34 -0300

    FEAT: add swiper component and crew store for product options

---
+++ src/stores/crew.js
@@ -0,0 +1,42 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+
+export const useCrewStore = defineStore('crew', () => {
+  // Aqui os dados de tripulação serão importados do arquivo JSON
+  const crews = ref([
+    {
+      id: 1,
+      name: 'Chapéus de Palha',
+    },
+    {
+      id: 2,
+      name: 'Barba Branca',
+    },
+    {
+      id: 3,
+      name: 'Donquixote',
+    },
+    {
+      id: 4,
+      name: 'Revolucionários',
+    },
+    {
+      id: 5,
+      name: 'Piratas do Barba Negra',
+    },
+    {
+      id: 6,
+      name: 'Piratas do Barba Branca',
+    },
+  ])
+
+  const currentCrew = ref(crews.value[0])
+
+  function setCurrentCrew(crewId) {
+    currentCrew.value = crews.value[crewId-1]
+    console.log(currentCrew.value)
+  }
+
+  return { crews, currentCrew, setCurrentCrew }
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { ProductCard, Header } from '@/components';
+import { ProductCard, Header, ProductsOptions } from '@/components';
 import router from '@/router';
 </script>
 
 <template>
   <Header />
+  <ProductsOptions />
   <ProductCard @click="router.push('/details/1')"/>
 </template>
